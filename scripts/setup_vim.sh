67f1c80b3b82540495b1afbadfc16d52c7ce8ab2
user <user@mail.com>
2025-08-09 15:11:39 -0400

    customized vim and made script to install vim-plug and set new path for vimrc

---
+++ scripts/setup_vim.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+
+echo "export VIMINIT='source $HOME/.config/vim/vimrc'" >> ${HOME}/.bash_profile
