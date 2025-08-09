4bddda2059e12f88397c8f729d9087d5095647e8
user <user@mail.com>
2025-08-09 11:25:58 -0400

    added configs for sway and wayland stuff

---
+++ sway/lock_screen.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# Credit to the following for comming up with this:
+# https://code.krister.ee/lock-screen-in-sway/
+# To Do: The fancier screen lock mentioned on that page might be cool to try.
+
+# Times the screen off and puts it to background
+swayidle \
+    timeout 10 'swaymsg "output * dpms off"' \
+    resume 'swaymsg "output * dpms on"' &
+
+# Locks the screen immediately
+swaylock \
+	--ignore-empty-password \
+	--daemonize \
+	--indicator-caps-lock \
+	--indicator \
+	--clock \
+	--timestr " %Hh %Mm %Ss"\
+	--datestr " %b-%d-%Y" \
+	--show-failed-attempts \
+	--indicator-idle-visible
+
+# Kills last background task so idle timer doesn't keep running
+kill %%
