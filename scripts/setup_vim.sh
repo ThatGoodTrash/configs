7a1c161858934cf5af1a1c296b707026555110e5
user <user@omen-laptop.lan>
2025-08-24 20:23:26 -0400

    modified some setup files from testing in fresh install. Added bash aliases

--- scripts/setup_vim.sh
+++ scripts/setup_vim.sh
@@ -4,4 +4,4 @@ curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
 
-echo "export VIMINIT='source $HOME/.config/vim/vimrc'" >> ${HOME}/.bash_profile
+echo "export VIMINIT='source $HOME/.config/vim/vimrc'" >> ${HOME}/.bashrc
